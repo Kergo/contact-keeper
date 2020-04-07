acc855c58b7a0991e40656a901ceb330110d9349
Kergo <sskamenov@gmail.com>
2020-04-07 02:00:12 +0200

    ContactForm Component & Add Contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -9,10 +9,10 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_FILTER,
 } from '../types';
 
-const ContactState = props => {
+const ContactState = (props) => {
   const initialState = {
     contacts: [
       {
@@ -20,28 +20,32 @@ const ContactState = props => {
         name: 'Sasho',
         email: 'sashs@sadas.coasd',
         phone: '232-231231',
-        type: 'personal'
+        type: 'personal',
       },
       {
         id: 2,
         name: 'Gosho',
         email: 'sashs@sadas.coasd',
         phone: '232-231231',
-        type: 'personal'
+        type: 'personal',
       },
       {
         id: 3,
         name: 'Tosho',
         email: 'sashs@sadas.coasd',
         phone: '232-231231',
-        type: 'professional'
+        type: 'professional',
       },
-    ]
+    ],
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
+  const addContact = (contact) => {
+    contact.id = 'asdasdasd'
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
 
@@ -56,10 +60,10 @@ const ContactState = props => {
   // Clear Filter
 
   return (
-    <ContactContext.Provider value={{contacts: state.contacts}}>
-      { props.children }
+    <ContactContext.Provider value={{ contacts: state.contacts, addContact }}>
+      {props.children}
     </ContactContext.Provider>
-  )
+  );
 };
 
 export default ContactState;
